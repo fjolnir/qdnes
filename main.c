343d2425ef991a9795e2b49d4a8f0573ef5709c2
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2014-02-12 23:37:02 +0900

    Added a little loop test

--- main.c
+++ main.c
@@ -10,15 +10,18 @@ int main(int argc, char *argv[])
     taot_init(&cpu);
 
     taot_dumpregs(&cpu);
-    fputs("Running program:\n", stderr);
 
     // Load a simple program (http://www.masswerk.at/6502/assembler.html)
+//    uint8_t program[] = {
+//        0xa2, 0x2,     // LDX #1
+//        0xac, 0x8, 0x0, // LDY $0
+//        0x8e, 0x0, 0x0, // STX $0
+//    };
     uint8_t program[] = {
-        0xa2, 0x1,     // LDX #1
-        0xac, 0x8, 0x0, // LDY $0
-        0x8e, 0x0, 0x0, // STX $0
+    0xA2, 0x00, 0xA9, 0x05, 0xE8, 0x8E, 0x00, 0x00, 0xCD, 0x00, 0x00, 0xD0, 0xF7
     };
     memcpy(cpu.mem + cpu.regs.pc, program, sizeof(program));
+    fprintf(stderr, "Running program (%d bytes):\n", sizeof(program));
 
     taot_dumpmem(&cpu, cpu.regs.pc, cpu.regs.pc + 31);
     fputs("\n---------------\n\n", stderr);
